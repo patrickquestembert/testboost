060e103340c08534ed5fc3c79ea623206c5cdc79
Patrick Questembert <patrick.questembert@gmail.com>
2014-07-01 17:09:05 -0400

    Adding XCode project

---
+++ TestBoost/TestBoost/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  TestBoost
+//
+//  Created by Patrick Questembert on 7/1/14.
+//  Copyright (c) 2014 DubLabs. All rights reserved.
+//
+
+#include <iostream>
+#include "boost/variant.hpp"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
